f60ff895ac46a0c57c19d8aaf068e4492cec4f96
mohsen12999 <mohsen1299@gmail.com>
2018-11-13 00:23:31 +0330

    make single page app

--- assets/js/pesarak.js
+++ assets/js/pesarak.js
@@ -8,115 +8,117 @@ var disableOptions = [];
 var saveAllChoises = [];
 
 var stages = [{
-		stageId: 1,
-		level: 1,
-		section: 1,
-		name: 'بیدار شدن',
-		desc: 'ساعت زنگ می زند. می خواهی چکار کنی؟',
-		background: "main.jpg",
-		options: [{
-				optionId: 1,
-				title: 'قطع زنگ و خوابیدن',
-				color: 'btn-outline-secondary',
-				event: {
-					nextStage: 2,
-					addedTime: 15,
-					addedEnergy: 20,
-					score: 10
-				}
-			},
-			{
-				optionId: 2,
-				title: ' بیدار شدن و شستن و شو',
-				color: 'btn-outline-warning',
-				event: {
-					nextStage: 10,
-					addedTime: 5,
-					addedEnergy: 0,
-					score: 15
-				}
-			}
-		]
+	stageId: 1,
+	level: 1,
+	section: 1,
+	name: 'بیدار شدن',
+	desc: 'ساعت زنگ می زند. می خواهی چکار کنی؟',
+	background: "main.jpg",
+	options: [{
+		optionId: 1,
+		title: 'قطع زنگ و خوابیدن',
+		color: 'btn-outline-secondary',
+		event: {
+			nextStage: 2,
+			addedTime: 15,
+			addedEnergy: 20,
+			score: 10
+		}
 	},
 	{
-		stageId: 2,
-		level: 1,
-		section: 1,
-		name: 'بیدار شدن',
-		desc: 'باز هم ساعت زنگ می زند. نمی خواهی بیدار شوی؟',
-		background: "main.jpg",
-		options: [{
-				optionId: 1,
-				title: 'قطع زنگ و خوابیدن',
-				color: 'btn-outline-secondary',
-				event: {
-					nextStage: 2,
-					addedTime: 15,
-					addedEnergy: 5,
-					score: 5
-				}
-			},
-			{
-				optionId: 2,
-				title: 'بیدار شدن',
-				color: 'btn-outline-warning',
-				event: {
-					nextStage: 10,
-					addedTime: 5,
-					addedEnergy: 0,
-					score: 15
-				}
-			}
-		]
-	},
-	{
-		stageId: 10,
-		level: 1,
-		section: 2,
-		name: 'آماده شدن',
-		desc: 'حالا که بیدار شدی می خواهی چکار کنی؟',
-		background: "main.jpg",
-		options: [{
-				optionId: 1,
-				title: 'مرتب کردن اتاق',
-				color: 'btn-outline-secondary',
-				event: {
-					nextStage: 11,
-					addedTime: 15,
-					addedEnergy: 5,
-					score: 5
-				}
-			},
-			{
-				optionId: 2,
-				title: 'خوردن صبحانه',
-				color: 'btn-outline-warning',
-				event: {
-					nextStage: 12,
-					addedTime: 5,
-					addedEnergy: 0,
-					score: 15
-				}
-			},
-			{
-				optionId: 2,
-				title: 'جمع کردن وسایل برای  مدرسه',
-				color: 'btn-outline-info',
-				event: {
-					nextStage: 13,
-					addedTime: 5,
-					addedEnergy: 0,
-					score: 15
-				}
-			}
-		]
+		optionId: 2,
+		title: ' بیدار شدن و شستن و شو',
+		color: 'btn-outline-warning',
+		event: {
+			nextStage: 10,
+			addedTime: 5,
+			addedEnergy: 0,
+			score: 15
+		}
 	}
+	]
+},
+{
+	stageId: 2,
+	level: 1,
+	section: 1,
+	name: 'بیدار شدن',
+	desc: 'باز هم ساعت زنگ می زند. نمی خواهی بیدار شوی؟',
+	background: "main.jpg",
+	options: [{
+		optionId: 1,
+		title: 'قطع زنگ و خوابیدن',
+		color: 'btn-outline-secondary',
+		event: {
+			nextStage: 2,
+			addedTime: 15,
+			addedEnergy: 5,
+			score: 5
+		}
+	},
+	{
+		optionId: 2,
+		title: 'بیدار شدن',
+		color: 'btn-outline-warning',
+		event: {
+			nextStage: 10,
+			addedTime: 5,
+			addedEnergy: 0,
+			score: 15
+		}
+	}
+	]
+},
+{
+	stageId: 10,
+	level: 1,
+	section: 2,
+	name: 'آماده شدن',
+	desc: 'حالا که بیدار شدی می خواهی چکار کنی؟',
+	background: "main.jpg",
+	options: [{
+		optionId: 1,
+		title: 'مرتب کردن اتاق',
+		color: 'btn-outline-secondary',
+		event: {
+			nextStage: 11,
+			addedTime: 15,
+			addedEnergy: 5,
+			score: 5
+		}
+	},
+	{
+		optionId: 2,
+		title: 'خوردن صبحانه',
+		color: 'btn-outline-warning',
+		event: {
+			nextStage: 12,
+			addedTime: 5,
+			addedEnergy: 0,
+			score: 15
+		}
+	},
+	{
+		optionId: 2,
+		title: 'جمع کردن وسایل برای  مدرسه',
+		color: 'btn-outline-info',
+		event: {
+			nextStage: 13,
+			addedTime: 5,
+			addedEnergy: 0,
+			score: 15
+		}
+	}
+	]
+}
 ];
 
 $(document).ready(function () {
 	//console.log('level1',level1);
-	stages = level1;
-	startGame();
+	//stages = level1;
+	//startGame();
+
+	showStartMenu();
 
 	//read level from file
 	// $.getJSON("level/level1.json", function (data) {
@@ -132,8 +134,150 @@ $(document).ready(function () {
 
 });
 
+function showStartMenu() {
+	$('div.card').css('background-image', "url('assets/image/main.jpg')");
+	$('.card-header').css('display', 'none');
+	$('.card-footer').css('display', 'none');
+	$('.card-body').html(`
+		<div class="row align-items-center">
+			<div class="col">
+				<img src="assets/image/pesarak.png"  alt="pesarak">
+			</div>
+		</div>
+		<div class="row align-items-center text-white" style="height: 120px">
+			<div id="content" class="col">
+				<h5 class="card-title"> پسرک </h5>
+				<p class="card-text">به بازی پسرک خوش آمدید</p>
+			</div>
+		</div>
+		<div class="row align-items-center" style="height: 80px">
+			<div class="col">
+				<button type="button" style="min-width: 150px" class="selectable btn btn-success btn-lg"
+					onClick="startGame()">
+					شروع بازی
+				</button>
+			</div>
+		</div>
+		<div class="row align-items-center" style="height: 100px">
+			<div class="col">
+				<button type="button" style="min-width: 150px" class="selectable btn btn-info btn-lg" onClick="showAbout()">
+					درباره ما
+				</button>
+			</div>
+		</div>
+	`);
+}
+
+function showAbout() {
+	$('div.card').css('background-image', "url('assets/image/main.jpg')");
+	$('.card-header').css('display', 'block');
+	$('.card-header').html(`
+		<h4> تیم توسعه بازی </h4>
+		<p> معرفی اعضای تیم مجیک لایت </p>
+	`);
+	$('.card-footer').css('display', 'block');
+	$('.card-body').html(`
+	<div class="row align-items-center team-card" style="height: 400px;">
+		<div class="col">
+			<div class="card text-center" style="width: 18rem;margin: auto;min-height: auto">
+				<img class="card-img-top" src="assets/image/amir.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title"> امیر سالار سلیمانی </h5>
+					<p class="card-text"> مدیر تیم و طراح بازی </p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row align-items-center team-card" style="height: 400px">
+		<div class="col">
+			<div class="card text-center" style="width: 18rem;margin: auto;min-height: auto">
+				<img class="card-img-top" src="assets/image/hosein.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title"> حسین فراهانی </h5>
+					<p class="card-text"> برنامه نویس </p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row align-items-center team-card" style="height: 400px">
+		<div class="col">
+			<div class="card text-center" style="width: 18rem;margin: auto;min-height: auto">
+				<img class="card-img-top" src="assets/image/mohsen.jpg" alt="Card image cap">
+				<div class="card-body">
+					<h5 class="card-title"> محسن شعبانیان </h5>
+					<p class="card-text"> برنامه نویس </p>
+				</div>
+			</div>
+		</div>
+	</div>
+	`);
+	$('.card-footer').css('display', 'block');
+	$('.card-footer').html(`
+	<button type="button" class="selectable btn btn-outline-secondary btn-lg btn-block" onClick="showStartMenu();">بازگشت به منو
+                            </button>
+	`);
+}
+
+function showScore(){
+	//TODO : Calculate game Score
+	$('.card-header').css('display', 'block');
+	$('.card-header').html(`
+		<h4> نتیجه بازی </h4>
+	`);
+	$('.card-body').html(`
+	<div id="content" class="row align-items-center" style="height: 400px">
+		<div class="col">
+			<h5 class="card-title">یک روز دیگر تمام شد</h5>
+			<p>ده درصد از زندگی‌تان وقایعی است که با آن روبه‌رو می‌شوید و نود درصدِ باقی‌مانده شیوه‌ی برخورد و پاسخ‌گویی به آنهاست</p>
+			<p style="text-align: left;">لو هولتر</p>
+			<p class="card-text" style="color: lightblue">امتیاز نهایی: 840</p>
+			<p class="card-text" style="color: lightgreen">تعداد تصمیمات درست: 14</p>
+			<p class="card-text" style="color: lightcoral">تعداد تصمیمات اشتباه: 6</p>
+		</div>
+	</div>
+	`);
+	$('.card-footer').css('display', 'block');
+	$('.card-footer').html(`
+	<button type="button" class="selectable btn btn-outline-secondary btn-lg btn-block" onClick="startGame()">
+	بازی دوباره </button>
+	<button type="button" class="selectable btn btn-outline-primary btn-lg btn-block" onClick="showStartMenu()">
+		منوی اصلی </button>
+	`);
+}
 
 function startGame() {
+
+	//TODO : start game efect
+
+	stages = level1;
+
+	$('.card-header').css('display', 'block');
+	$('.card-header').html(`
+		<nav class="nav nav-pills nav-justified">
+			<a class="nav-item nav-link disabled" href="#">
+				<div>انرژی</div>
+				<div id="energy">48</div>
+			</a>
+			<a class="nav-item nav-link disabled" href="#">
+				<div>امتیاز</div>
+				<div id="totalScore">0</div>
+			</a>
+			<a class="nav-item nav-link disabled" href="#">
+				<div>ساعت</div>
+				<div id="time">8:00</div>
+			</a>
+		</nav>
+	`);
+	$('.card-body').html(`
+		<div id="content" class="row align-items-center" style="height: 400px">
+			<div class="col">
+				<h5 class="card-title"></h5>
+				<p class="card-text"></p>
+			</div>
+		</div>
+	`);
+	$('.card-footer').css('display', 'block');
+
 	totalScore = 0;
 	time = 700;
 	energy = 50;
@@ -163,7 +307,7 @@ function makeStage() {
 	$('#content .card-title').html(currentStage.name);
 	$('#content .card-text').html(currentStage.desc);
 	$('div.card').css('background-image', "url('assets/image/" + currentStage.background + "')");
-	
+
 	$('div.card').fadeIn(500)
 
 	$('.card-footer').empty();
@@ -196,9 +340,10 @@ function onClickOption(opId) {
 		// add to disable option
 		disableOptions.push(opId);
 		//console.log(disableOptions);
-	} else if(event.nextStage == -1){
-		window.location.href = 'score.html';
-	}else{
+	} else if (event.nextStage == -1) {
+		//window.location.href = 'score.html';
+		showScore();
+	} else {
 		disableOptions = [];
 	}
 
