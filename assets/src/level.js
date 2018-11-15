a06c6ba9f14eedab0737ea6352ed155a217ab2fe
mohsen12999 <mohsen1299@gmail.com>
2018-11-15 23:58:39 +0330

    change level

--- assets/src/level.js
+++ assets/src/level.js
@@ -366,7 +366,7 @@ var levelInfo = [{
                 title: "صحبت با دوستان",
                 color: "btn-outline-warning",
                 event: {
-                    nextStage: 31,
+                    nextStage: 33,
                     addedTime: 15,
                     addedEnergy: -10,
                     score: 10
@@ -377,7 +377,7 @@ var levelInfo = [{
                 title: "نشستن یک گوشه",
                 color: "btn-outline-danger",
                 event: {
-                    nextStage: 31,
+                    nextStage: 34,
                     addedTime: 15,
                     addedEnergy: 0,
                     score: 5
@@ -388,7 +388,155 @@ var levelInfo = [{
     {
         stageId: 31,
         level: 4,
-        section: 1,
+        section: 2,
+        name: "در مدرسه",
+        desc: " در هنگام بازی با بچه های دیگه یکی جر می زنه",
+        background: "school.jpg",
+        options: [{
+                optionId: 1,
+                title: "تو هم جر می زنی",
+                color: "btn-outline-secondary",
+                event: {
+                    nextStage: 32,
+                    addedTime: 5,
+                    addedEnergy: -10,
+                    score: -10
+                }
+            },
+            {
+                optionId: 2,
+                title: "جلوی دیگران تحقیرش می کنی",
+                color: "btn-outline-info",
+                event: {
+                    nextStage: 32,
+                    addedTime: 5,
+                    addedEnergy: -10,
+                    score: -5
+                }
+            },
+            {
+                optionId: 3,
+                title: "با مشت بهش حمله می کنی",
+                color: "btn-outline-secondary",
+                event: {
+                    nextStage: 32,
+                    addedTime: 5,
+                    addedEnergy: -30,
+                    score: -25
+                }
+            },
+            {
+                optionId: 4,
+                title: "گذشت کنی",
+                color: "btn-outline-danger",
+                event: {
+                    nextStage: 36,
+                    addedTime: 5,
+                    addedEnergy: 0,
+                    score: 25
+                }
+            },
+            {
+                optionId: 5,
+                title: "از بازی بکشی کنار",
+                color: "btn-outline-primary",
+                event: {
+                    nextStage: 36,
+                    addedTime: 5,
+                    addedEnergy: 0,
+                    score: -25
+                }
+            }
+        ]
+    },
+    {
+        stageId: 32,
+        level: 4,
+        section: 3,
+        name: "در مدرسه",
+        desc: "بین چند تا از بچه ها زد و خورد راه افتاده!",
+        background: "school.jpg",
+        options: [{
+                optionId: 1,
+                title: "به مدیر یا ناظم خبر می دهم",
+                color: "btn-outline-secondary",
+                event: {
+                    nextStage: 36,
+                    addedTime: 5,
+                    addedEnergy: -10,
+                    score: 20
+                }
+            },
+            {
+                optionId: 2,
+                title: "سعی می کنم از هم جدایشان کنم",
+                color: "btn-outline-warning",
+                event: {
+                    nextStage: 36,
+                    addedTime: 5,
+                    addedEnergy: -10,
+                    score: 10
+                }
+            },
+            {
+                optionId: 3,
+                title: "فقط تماشا می کنم",
+                color: "btn-outline-primary",
+                event: {
+                    nextStage: 36,
+                    addedTime: 5,
+                    addedEnergy: 0,
+                    score: 0
+                }
+            }
+        ]
+    },
+    {
+        stageId: 33,
+        level: 4,
+        section: 4,
+        name: "در مدرسه",
+        desc: " بحثی در مورد کسی در جمع دوستان می شود؟",
+        background: "school.jpg",
+        options: [{
+                optionId: 1,
+                title: "تو هم مثل دیگران پشت سرش صحبت می کنی",
+                color: "btn-outline-secondary",
+                event: {
+                    nextStage: 36,
+                    addedTime: 5,
+                    addedEnergy: -10,
+                    score: -30
+                }
+            },
+            {
+                optionId: 2,
+                title: "بحث رو عوض کنی",
+                color: "btn-outline-danger",
+                event: {
+                    nextStage: 36,
+                    addedTime: 5,
+                    addedEnergy: -10,
+                    score: 15
+                }
+            },
+            {
+                optionId: 3,
+                title: "هیچی نگی",
+                color: "btn-outline-warning",
+                event: {
+                    nextStage: 36,
+                    addedTime: 5,
+                    addedEnergy: 0,
+                    score: 0
+                }
+            }
+        ]
+    },
+    {
+        stageId: 36,
+        level: 4,
+        section: 5,
         name: "سر کلاس",
         desc: "معلم اعلام می کنه تکالیفتون رو تحویل بدین!",
         background: "school.jpg",
@@ -397,7 +545,7 @@ var levelInfo = [{
                 title: "تکالیفت رو تحویل بدی",
                 color: "btn-outline-secondary",
                 event: {
-                    nextStage: 32,
+                    nextStage: 38,
                     addedTime: 5,
                     addedEnergy: -5,
                     score: 50,
@@ -413,7 +561,7 @@ var levelInfo = [{
                 title: "بگی تکلیف ننوشتم",
                 color: "btn-outline-warning",
                 event: {
-                    nextStage: 32,
+                    nextStage: 38,
                     addedTime: 5,
                     addedEnergy: -5,
                     score: 20
@@ -424,7 +572,7 @@ var levelInfo = [{
                 title: "به خاطر رفتن به دستشویی کلاس رو بپیچونی!",
                 color: "btn-outline-danger",
                 event: {
-                    nextStage: 32,
+                    nextStage: 38,
                     addedTime: 5,
                     addedEnergy: -10,
                     score: 10
@@ -433,155 +581,7 @@ var levelInfo = [{
         ]
     },
     {
-        stageId: 32,
-        level: 4,
-        section: 2,
-        name: "در مدرسه",
-        desc: "بین چند تا از بچه ها زد و خورد راه افتاده!",
-        background: "school.jpg",
-        options: [{
-                optionId: 1,
-                title: "به مدیر یا ناظم خبر می دهم",
-                color: "btn-outline-secondary",
-                event: {
-                    nextStage: 33,
-                    addedTime: 5,
-                    addedEnergy: -10,
-                    score: 20
-                }
-            },
-            {
-                optionId: 2,
-                title: "سعی می کنم از هم جدایشان کنم",
-                color: "btn-outline-warning",
-                event: {
-                    nextStage: 33,
-                    addedTime: 5,
-                    addedEnergy: -10,
-                    score: 10
-                }
-            },
-            {
-                optionId: 3,
-                title: "فقط تماشا می کنم",
-                color: "btn-outline-primary",
-                event: {
-                    nextStage: 33,
-                    addedTime: 5,
-                    addedEnergy: 0,
-                    score: 0
-                }
-            }
-        ]
-    },
-    {
-        stageId: 33,
-        level: 4,
-        section: 3,
-        name: "در مدرسه",
-        desc: " در هنگام بازی با بچه های دیگه یکی جر می زنه",
-        background: "school.jpg",
-        options: [{
-                optionId: 1,
-                title: "تو هم جر می زنی",
-                color: "btn-outline-secondary",
-                event: {
-                    nextStage: 33,
-                    addedTime: 5,
-                    addedEnergy: -10,
-                    score: -10
-                }
-            },
-            {
-                optionId: 2,
-                title: "جلوی دیگران تحقیرش می کنی",
-                color: "btn-outline-info",
-                event: {
-                    nextStage: 34,
-                    addedTime: 5,
-                    addedEnergy: -10,
-                    score: -5
-                }
-            },
-            {
-                optionId: 3,
-                title: "با مشت بهش حمله می کنی",
-                color: "btn-outline-secondary",
-                event: {
-                    nextStage: 34,
-                    addedTime: 5,
-                    addedEnergy: -30,
-                    score: -25
-                }
-            },
-            {
-                optionId: 4,
-                title: "گذشت کنی",
-                color: "btn-outline-danger",
-                event: {
-                    nextStage: 34,
-                    addedTime: 5,
-                    addedEnergy: 0,
-                    score: 25
-                }
-            },
-            {
-                optionId: 5,
-                title: "از بازی بکشی کنار",
-                color: "btn-outline-primary",
-                event: {
-                    nextStage: 34,
-                    addedTime: 5,
-                    addedEnergy: 0,
-                    score: -25
-                }
-            }
-        ]
-    },
-    {
-        stageId: 34,
-        level: 4,
-        section: 4,
-        name: "در مدرسه",
-        desc: " بحثی در مورد کسی در جمع دوستان می شود؟",
-        background: "school.jpg",
-        options: [{
-                optionId: 1,
-                title: "تو هم مثل دیگران پشت سرش صحبت می کنی",
-                color: "btn-outline-secondary",
-                event: {
-                    nextStage: 35,
-                    addedTime: 5,
-                    addedEnergy: -10,
-                    score: -30
-                }
-            },
-            {
-                optionId: 2,
-                title: "بحث رو عوض کنی",
-                color: "btn-outline-danger",
-                event: {
-                    nextStage: 35,
-                    addedTime: 5,
-                    addedEnergy: -10,
-                    score: 15
-                }
-            },
-            {
-                optionId: 3,
-                title: "هیچی نگی",
-                color: "btn-outline-warning",
-                event: {
-                    nextStage: 35,
-                    addedTime: 5,
-                    addedEnergy: 0,
-                    score: 0
-                }
-            }
-        ]
-    },
-    {
-        stageId: 35,
+        stageId: 38,
         level: 4,
         section: 5,
         name: "در مدرسه",
